3978d4c7cf3c00e11b7d67a4aeae3c80482e3701
Steve Martell <martell.steve@gmail.com>
2016-10-12 12:43:53 -0700

    deleted some redundant files

--- AlaskaHerring/src/ham.cpp
+++ AlaskaHerring/src/ham.cpp
@@ -997,8 +997,10 @@ void model_parameters::calcSpawningStockRecruitment()
 	// Ricker stock-recruitment function 
 	// so = reck/phiE; where reck > 1.0
 	// beta = log(reck)/(ro * phiE)
+	// Beverton Holt use:
+	// beta = (reck - 1.0)/(ro *phiE)
 	ro   = mfexp(log_ro);
-	reck = mfexp(log_reck);
+	reck = mfexp(log_reck) + 1.0;
 	so   = reck/phie;
 	beta = log(reck) / (ro * phie);
 	spawners = ssb(mod_syr,mod_nyr-sage+1).shift(rec_syr);
